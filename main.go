fb6c7cea312a902ba4c44ad789c1867dbbea1227
joao rocha <jp.rcha@gmail.com>
2026-09-07 03:23:09 -0300

    read and play files from a folder

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//	"github.com/gopxl/beep"
+	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/mp3"
 	"github.com/gopxl/beep/speaker"
 	"log"
@@ -10,9 +10,42 @@ import (
 	"time"
 )
 
+/*
+TODO - JSON
+blacklist
+whitelist
+sounds folder
+if key pressed plays sound
+global random time ratio
+*/
+
+// gets all sound effects file names
+func FileNames() []string {
+	files, err := os.ReadDir("sfx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fileNames []string
+
+	for _, file := range files {
+		if !file.IsDir() {
+			fileNames = append(fileNames, file.Name())
+		}
+	}
+	return fileNames
+}
+
 func main() {
-	fmt.Print("hello world!")
-	file, err := os.Open("sfx.mp3")
+	fmt.Println("hello world!")
+
+	sfx := FileNames()
+	fmt.Println("----- sounds list -----")
+	for i, s := range sfx {
+		fmt.Printf("%d - %s\n", i+1, s)
+	}
+
+	file, err := os.Open("sfx/" + sfx[0])
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,7 +59,10 @@ func main() {
 	sr := format.SampleRate * 1 //audio speed
 	speaker.Init(sr, sr.N(time.Second/10))
 
-	speaker.Play(streamer)
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
 
-	select {}
+	<-done
 }
